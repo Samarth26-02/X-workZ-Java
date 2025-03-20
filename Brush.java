fb93041f9c2ce74f7671f60b20fd6e1c83dd2c5f
T S Samarth <tssam1547@gmail.com>
2025-03-20 21:58:41 +0530

    Add files via upload

---
+++ Brush.java
@@ -0,0 +1,12 @@
+public class Brush {
+    Color color;
+    Brush(Color color){
+        this.color=color;
+    }
+    public void clean(){
+        System.out.println("running clean in Brush");
+    }
+    public void scrub(){
+        System.out.println("Running scrub in Brush");
+    }
+}
