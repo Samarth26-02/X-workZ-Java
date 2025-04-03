702b4cd6a1fe04abdfb647eb9d047f52b33e30bf
T S Samarth <tssam1547@gmail.com>
2025-04-03 09:36:07 +0530

    inheritance

---
+++ src/com/xworkz/inheritance/internal/Airplane.java
@@ -0,0 +1,8 @@
+package com.xworkz.inheritance.internal;
+
+public class Airplane extends com.xworkz.inheritance.internal.Vehicle {
+    public Airplane() {
+        super();
+        System.out.println("Constructor in Airplane");
+    }
+}
---
+++ src/inheritance/src/com/xworkz/inheritance/internal/AI.java
@@ -0,0 +1,8 @@
+package inheritance.src.com.xworkz.inheritance.internal;
+
+public class AI extends Subject {
+    public AI() {
+        super();
+        System.out.println("Constructor in AI");
+    }
+}
