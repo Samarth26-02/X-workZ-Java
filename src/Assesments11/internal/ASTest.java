b16105bb8637adaa63902f8a61700d0b2d2f3ca3
T S Samarth <tssam1547@gmail.com>
2025-04-08 19:49:56 +0530

    ASTest.java

--- src/Assesments11/internal/ASTest.java
+++ src/Assesments11/internal/ASTest.java
@@ -1,6 +1,7 @@
 package Assesments11.internal;
 
 public class ASTest {
+    //writing a no-argument constructor
     protected ASTest(){
         System.out.println("no-arg constructor in ASTest");
     }
