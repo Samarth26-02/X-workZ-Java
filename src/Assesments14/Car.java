5abd8b4569d54b0ba5fb67313334e8f00c6c0d74
T S Samarth <tssam1547@gmail.com>
2025-04-14 19:37:51 +0530

    final keyword

---
+++ src/Assesments14/Car.java
@@ -0,0 +1,20 @@
+package Assesments14;
+
+public class Car extends Vehicle{
+    public Car(int wheels, int capacity) {
+        super(wheels, capacity);
+    }
+
+    // This would cause an error if uncommented, since displayDetails is final
+    /*
+    @Override
+    public void displayDetails() {
+        System.out.println("This is a car.");
+    }
+    */
+
+    @Override
+    public void fuelType() {
+        System.out.println("Petrol.");
+    }
+}
---
+++ src/Assesments14/Main.java
@@ -0,0 +1,9 @@
+package Assesments14;
+
+public class Main {
+    public static void main(String[] args) {
+        Car myCar = new Car(4, 5);
+        myCar.displayDetails();  // Final method from Vehicle
+        myCar.fuelType();        // Overridden method in Car
+    }
+}
