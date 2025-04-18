0e75ab934d665b248b767de45baf2ffac2ecc866
T S Samarth <tssam1547@gmail.com>
2025-04-18 18:50:03 +0530

    Armstrong number

--- src/InterviewQustions/ArmstrongRunner.java
+++ src/InterviewQustions/ArmstrongRunner.java
@@ -4,7 +4,7 @@ public class ArmstrongRunner {
     public static void main(String[] args) {
         ArmstrongNo armstrongNo=new ArmstrongCheck();
 
-        int number=133;
+        int number=153;//Armstrong numbers are:1,153,370,371,407,1634,8208,9474
         if(armstrongNo.isArmstrong(number)){
             System.out.println(number+" is an Armstrong number");
         }
