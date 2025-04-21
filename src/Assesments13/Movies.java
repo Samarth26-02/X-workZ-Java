3cd73c1bf51730003fda626fd97d9fc7f0526da7
T S Samarth <tssam1547@gmail.com>
2025-04-21 11:53:15 +0530

    Generics

---
+++ src/Assesments13/Movies.java
@@ -0,0 +1,15 @@
+package Assesments13;
+
+public class Movies<T,C> {
+    T name;
+    T actor;
+    C releaseDate;
+
+    public Movies(T name,T actor,C releaseDate){
+        this.name=name;
+        this.actor=actor;
+        this.releaseDate=releaseDate;
+
+        System.out.println("Movie name:"+name+" Actor:"+actor+" Release Date:"+releaseDate);
+    }
+}
---
+++ src/Assesments13/MoviesRunner.java
@@ -0,0 +1,7 @@
+package Assesments13;
+
+public class MoviesRunner {
+    public static void main(String[] args) {
+        Movies<String,Integer> movies=new Movies<>("Pirates of the caribbean","Johny Depp",2003);
+    }
+}
